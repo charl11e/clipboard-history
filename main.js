1b0195421ed2ea12f8f1b71b4f0c8350f7c5dfeb
charl11e <146877892+charl11e@users.noreply.github.com>
2025-09-22 03:17:34 +0100

    Keyboard history now works and gets printed to console for timebeing

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { app, BrowserWindow, clipboard } = require('electron');
-const { path } = require('path');
+const path = require('path');
 
 let lastText = '';
 let history = [];
@@ -19,10 +19,42 @@ function createWindow() {
             preload: path.join(__dirname, 'preload.js'),
         }
     });
-    
+
     mainWindow.loadFile(path.join(__dirname, 'renderer', 'index.html'));
 }
 
+function startClipboardWatcher() {
+    setInterval(() => {
+        const currentText = clipboard.readText();
+        if (!currentText || currentText === lastText) {
+            return;
+        };
+        lastText = currentText;
+        
+        if (history.indexOf(currentText) !== -1) {
+            history = history.filter(item => item !== currentText);
+        };
+        history.unshift(currentText);
+        if (history.length > MAX_ITEMS) {
+            history.pop();
+        };
+
+        console.log(currentText, history);
+    }, POLL_TIME);
+}
 app.whenReady().then(() => {
     createWindow();
+    startClipboardWatcher();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    })
 })
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
